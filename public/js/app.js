d72a99ffa53bcfc7b26daacb921960e066f4fc48
Dennis lam <denwt.lam@outlook.com>
2021-08-05 22:48:16 -0400

    add voice synthesis for coordinates

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const coordinateForm = document.querySelector('form');
 const coordinate = document.querySelector('#coordinate');
+const button = document.getElementById('generator');
 
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
@@ -8,5 +9,24 @@ coordinateForm.addEventListener('click', (event) => {
 
     var column = Math.floor(Math.random() * 8);
     var row = Math.floor(Math.random() * 8) + 1;
-    coordinate.textContent = alphabet[column] + row.toString();
+    var text = alphabet[column] + row.toString();
+    coordinate.textContent = text;
+
+    if (column == 0){
+        playText("a-" + row.toString());
+        // playText(text);
+    }else{
+        playText(text);
+    }
+    
 })
+
+function playText(text){
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 3;
+    // utterance.addEventListener('end', () => {
+    //     button.disabled = false;
+    // })
+    // button.disabled = true;
+    speechSynthesis.speak(utterance);
+}
